aa6843129c07105e932b55898971abe4acfd96b8
vsmiian <vetas91@gmail.com>
2024-01-05 11:00:58 +0100

    Add iexcloud; add fetch stock price

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iexcloud[:public],
+      secret_token: Rails.application.credentials.iexcloud[:secret],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+    client.price(ticker)
+  end
+
 end
